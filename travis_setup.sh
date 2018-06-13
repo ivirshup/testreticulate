e3212a8cebf210720607d36d2d2e20bac4dcf86b
Isaac Virshup <ivirshup@gmail.com>
2018-06-13 18:20:07 +1000

    is how you use conda/ reticulate os specific?

--- travis_setup.sh
+++ travis_setup.sh
@@ -2,6 +2,7 @@
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
+    export RETICULATE_PYTHON="$HOME/miniconda/bin/python"
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
 fi
 bash miniconda.sh -b -p $HOME/miniconda
