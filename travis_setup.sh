09b758d00f65edd1a1a42ef8a357c7c6fe1a4f48
Isaac Virshup <ivirshup@gmail.com>
2018-06-13 20:14:35 +1000

    Copy what seurat does

--- travis_setup.sh
+++ travis_setup.sh
@@ -1,15 +1,25 @@
 
+# install python
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
-    export RETICULATE_PYTHON="$HOME/miniconda/bin/python"
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
 fi
+
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
+# export RETICULATE_PYTHON="$HOME/miniconda/bin/python"
 hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
+# source activate base
 conda info -a
-# conda install numpy
-# pip install scanpy
+# Scanpy dependencies
+conda install -q numpy seaborn scikit-learn statsmodels numba
+pip install --upgrade pip
+pip install scanpy
+pip install phate
+echo "check python installation"
+pip list
+conda list
+which python
