34dc51a124dec6f49709cdc4ba70bc7d7339c574
Isaac Virshup <ivirshup@gmail.com>
2018-06-14 20:18:26 +1000

    Add case for windows

--- tests/testthat.R
+++ tests/testthat.R
@@ -8,11 +8,15 @@ if (!("rettest" %in% conda_list()$name)) {
     conda_create("rettest")
     print("Install")
     conda_install("rettest", c("python=3.6", "numpy", "seaborn", "scikit-learn", "statsmodels", "numba", "cython"), forge=F)
-    print("Install: scanpy")
-    if (Sys.info()[["sysname"]] == "Linux") {
+    sysname = Sys.info()[["sysname"]]
+    if (sysname == "Linux") {
         conda_install("rettest", c("scanpy", "phate"), pip=T)
-    } else if (Sys.info()[["sysname"]] == "Darwin") {
+    } else if (sysname == "Darwin") {
         conda_install("rettest", c("scanpy", "phate"), pip=T, pip_ignore_installed=F)
+    } else if (sysname == "Windows") {
+        conda_install("rettest", c("scanpy", "phate"), pip=T, pip_ignore_installed=F)
+    } else {
+        stop("Error: Current OS (", sysname, ") is not supported")
     }
     print("Activate")
     use_condaenv("rettest")
